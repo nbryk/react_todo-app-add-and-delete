d384a4ad3205c58aa1037478d9fb7bc8354443d2
Nadiia Bryk <25nadin47@gmail.com>
2025-06-12 19:09:17 +0300

    separate header

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { FilterStatus } from './types/FilterStatus';
 import { USER_ID } from './constants';
 import classNames from 'classnames';
 import { ErrorNotification } from './components/ErrorNotification';
+import { TodoHeader } from './components/TodoHeader';
 
 export const App: React.FC = () => {
   const [todosFromServer, setTodosFromServer] = useState<Todo[]>([]);
@@ -175,28 +176,13 @@ export const App: React.FC = () => {
       <h1 className="todoapp__title">todos</h1>
 
       <div className="todoapp__content">
-        <header className="todoapp__header">
-          {/* this button should have `active` class only if all todos are completed */}
-          <button
-            type="button"
-            className="todoapp__toggle-all"
-            data-cy="ToggleAllButton"
-          />
-
-          {/* Add a todo on form submit */}
-          <form onSubmit={handleSubmit}>
-            <input
-              ref={inputRef}
-              data-cy="NewTodoField"
-              type="text"
-              className="todoapp__new-todo"
-              placeholder="What needs to be done?"
-              value={newTodoTitle}
-              onChange={handleNewTodoTitleChange}
-              disabled={isAdding}
-            />
-          </form>
-        </header>
+        <TodoHeader
+          newTodoTitle={newTodoTitle}
+          isAdding={isAdding}
+          inputRef={inputRef}
+          onNewTodoTitleChange={handleNewTodoTitleChange}
+          onSubmit={handleSubmit}
+        />
 
         <section className="todoapp__main" data-cy="TodoList">
           {filteredTodos.map(todo => (
@@ -339,3 +325,26 @@ export const App: React.FC = () => {
     </div>
   );
 };
+
+// {/* <header className="todoapp__header">
+//           {/* this button should have `active` class only if all todos are completed */}
+//           <button
+//             type="button"
+//             className="todoapp__toggle-all"
+//             data-cy="ToggleAllButton"
+//           />
+
+//           {/* Add a todo on form submit */}
+//           <form onSubmit={handleSubmit}>
+//             <input
+//               ref={inputRef}
+//               data-cy="NewTodoField"
+//               type="text"
+//               className="todoapp__new-todo"
+//               placeholder="What needs to be done?"
+//               value={newTodoTitle}
+//               onChange={handleNewTodoTitleChange}
+//               disabled={isAdding}
+//             />
+//           </form>
+//         </header> */}
